990db46defcfeeb9b25cda8eb9062f020d49f866
Cawa <Cawa@CAWAKA>
2023-10-30 21:51:58 +0300

    Сделано задание 1. По запросу https://localhost:5001/Home/Index отображается список товаров, которые вы хотите продавать.

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -13,9 +13,21 @@ namespace WebApplication1.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index(int a, int b)
+        public string Index()
         {
-            return View();
+            var result = string.Empty;
+            var listProducts = new List<Product>()
+            {
+                new Product(1, 11, "Name1", "Descr1"),
+                new Product(2, 22, "Name2", "Descr2"),
+                new Product(3, 33, "Name3", "Descr3"),
+            };
+            
+            foreach (var product in listProducts)
+            {
+                result += product;
+            }
+            return result;
         }
 
         public IActionResult Privacy()
---
+++ WebApplication1/Controllers/Product.cs
@@ -0,0 +1,24 @@
+﻿namespace WebApplication1.Controllers
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Cost { get; set; }
+        public string Description { get; set; }
+
+        public Product(int id, int cost, string name, string description)
+        {
+            Id = id;
+            Name = name;
+            Cost = cost;
+            Description = description;
+        }
+
+        public override string ToString()
+        {
+            return $"{Id}\n{Name}\n{Cost}\n\n";
+        }
+
+    }
+}
