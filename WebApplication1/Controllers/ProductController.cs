389b69ed1a3917cf17634662c29a3ae0e35c23e5
Cawa <Cawa@CAWAKA>
2023-11-01 14:03:10 +0300

    123

--- WebApplication1/Controllers/ProductController.cs
+++ WebApplication1/Controllers/ProductController.cs
@@ -2,11 +2,34 @@
 
 namespace WebApplication1.Controllers
 {
-    public class ProductController1 : Controller
+    public class ProductController : Controller
     {
-        public IActionResult Index()
+
+        public string Index(string id)
         {
-            return View();
+            var listProducts = new List<Product>()
+            {
+                new Product(1, 11, "Name1", "Descr1"),
+                new Product(2, 22, "Name2", "Descr2"),
+                new Product(3, 33, "Name3", "Descr3"),
+            };
+            if (int.TryParse(id, out var productId))
+            {
+                try
+                {
+                    var result = listProducts.Where(x => x.Id == productId).Single();
+                    return result.ToString();
+                }
+                catch (Exception eex)
+                {
+                    return $"{eex.Message}";
+                }
+            }
+            else
+            {
+                return $"Неверный формат Id";
+            }
+            return string.Empty;
         }
     }
 }
