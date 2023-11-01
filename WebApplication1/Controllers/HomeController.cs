2b7778d19af1acce9f3c64ad577644d5f0564bb4
Cawa <Cawa@CAWAKA>
2023-11-01 14:02:29 +0300

    Create ProductController

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -41,34 +41,5 @@ namespace WebApplication1.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
-    public class ProductController : Controller
-    {
-
-        public string Index(string id)
-        {
-            var listProducts = new List<Product>()
-            {
-                new Product(1, 11, "Name1", "Descr1"),
-                new Product(2, 22, "Name2", "Descr2"),
-                new Product(3, 33, "Name3", "Descr3"),
-            };
-            if (int.TryParse(id, out var productId))
-            {
-                try
-                {
-                    var result = listProducts.Where(x => x.Id == productId).Single();
-                    return result.ToString();
-                }
-                catch (Exception eex)
-                {
-                    return $"{eex.Message}";
-                }
-            }
-            else
-            {
-                return $"Неверный формат Id";
-            }
-            return string.Empty;
-        }
-    }
+   
 }
