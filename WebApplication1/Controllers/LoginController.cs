3973e6997e0cbc2c1c42f6b772c1ecd05e4bb38a
Cawkuncon <homenet10@yandex.ru>
2023-11-17 11:15:17 +0300

    сделано задание 5.2

---
+++ WebApplication1/Controllers/LoginController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+	public class LoginController : Controller
+	{
+		public IActionResult Login()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public string TryToLogin(IUserInfo userInfo)
+		{
+			return ($"{userInfo.Name} {userInfo.Password} ");
+		}
+	}
+}
---
+++ WebApplication1/Models/UserInfo.cs
@@ -0,0 +1,17 @@
+﻿
+namespace WebApplication1.Models
+{
+	public class UserInfo : IUserInfo
+	{
+		public string Name { get ; set; }
+		public string Password { get ; set; }
+		//public string SaveUser { get; set; }
+	}
+
+	public interface IUserInfo
+	{
+		string Name { get; set; }
+		string Password { get; set; }
+		//string SaveUser { get; set; }
+	}
+}
