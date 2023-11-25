ec483ed88dbc2967426872602799dbfcce48c6b7
Cawa <Cawa@CAWAKA>
2023-11-25 15:37:18 +0300

    сделано задание 5_3

---
+++ WebApplication1/Controllers/RegisterController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class RegisterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public string RegisterUser(UserReg user)
+        {
+            return $"{user.Name} {user.Password} {user.PassCheck()}";
+        }
+    }
+}
---
+++ WebApplication1/Models/UserReg.cs
@@ -0,0 +1,23 @@
+﻿namespace WebApplication1.Models
+{
+    public class UserReg : IUserReg
+    {
+        public string Name { get; set; }
+
+        public string Password { get; set; }
+
+        public string Password2 { get; set; }
+
+        public bool PassCheck()
+        {
+            return Password == Password2 && Password != null;
+        }
+    }
+
+    public interface IUserReg
+    {
+        string Name { get; set; }
+        string Password { get; set; }
+        string Password2 { get; set; }
+    }
+}
