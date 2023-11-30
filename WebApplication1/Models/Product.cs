30f956bb9ab8c8b0f3e5d2aa064126d6f620a54a
Cawa <Cawa@CAWAKA>
2023-11-30 19:40:28 +0300

    Валидация при добавлении товара и его изменении. Задание 6.3

--- WebApplication1/Models/Product.cs
+++ WebApplication1/Models/Product.cs
@@ -1,9 +1,18 @@
-﻿namespace WebApplication1.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
 {
     public class Product
     {
         public int Id { get; set; }
+
+
+        [Required(ErrorMessage = "Не указано имя товара")]
+        [StringLength(20, MinimumLength = 2, ErrorMessage = "Имя товара должно содержать от 2 до 30 символов")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Не указана цена товара")]
+        [Range(1, 1000000, ErrorMessage = "Укажите цену товара в диапазоне от 1 до 1000000")]
         public int Cost { get; set; }
         public string Description { get; set; }
         public int Count { get; set; }
