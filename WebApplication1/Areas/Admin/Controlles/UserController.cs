0df6f24a38726d89dc222db229ce4a9a36b876ce
Cawa <Cawa@CAWAKA>
2023-12-10 19:27:23 +0300

    Добавлена кнопка действие. Функционал удаления пользователя в кнопке.

---
+++ WebApplication1/Areas/Admin/Controlles/UserController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Areas.Admin.Controlles
+{
+    public class UserController : Controller
+    {
+        private readonly IUserRepository UsersRepository;
+
+        public UserController(IUserRepository usersRepository)
+        {
+            UsersRepository = usersRepository;
+        }
+
+        public IActionResult Index(UserReg user)
+        {
+            UsersRepository.DeleteUser(user);
+            return RedirectToAction("Index", "Home", new { Area = "Admin" });
+        }
+    }
+}
--- WebApplication1/Models/UserRepository.cs
+++ WebApplication1/Models/UserRepository.cs
@@ -9,6 +9,11 @@
             Users.Add(user);
         }
 
+        public void DeleteUser(UserReg user)
+        {
+            Users.Remove(user);
+        }
+
         public UserReg GetUser(string Name)
         {
             var user = Users.FirstOrDefault(x => x.Name == Name);
@@ -25,6 +30,7 @@
     {
         public void Add(UserReg user);
         public UserReg GetUser(string Name);
-        public List<UserReg> GetUsers();        
+        public List<UserReg> GetUsers();
+        public void DeleteUser(UserReg user);
     }
 }
