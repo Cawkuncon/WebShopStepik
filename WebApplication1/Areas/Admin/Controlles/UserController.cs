163b008fa565ebb27463ca0ade0d3fc8aecb0ec1
Cawa <Cawa@CAWAKA>
2023-12-11 21:31:35 +0300

    Редактирование данных пользователя.

--- WebApplication1/Areas/Admin/Controlles/UserController.cs
+++ WebApplication1/Areas/Admin/Controlles/UserController.cs
@@ -25,5 +25,15 @@ namespace WebApplication1.Areas.Admin.Controlles
             var user = UsersRepository.GetUser(Name);
             return View(user);
         }
+
+        [HttpPost]
+        public IActionResult EditUserInfo(UserReg user, string Name)
+        {
+            var us = UsersRepository.GetUser(Name);
+            us.Number = user.Number;
+            us.Age = user.Age;
+            us.Email = user.Email;
+            return RedirectToAction("UserInfoCheck", "Home", new { Area = "Admin", Name = Name });
+        }
     }
 }
