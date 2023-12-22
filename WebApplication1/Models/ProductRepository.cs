10feb1fdd2009d8b675474c129146c561fa6a371
Cawa <Cawa@CAWAKA>
2023-12-22 20:57:49 +0300

    главная страница

--- WebApplication1/Models/ProductRepository.cs
+++ WebApplication1/Models/ProductRepository.cs
@@ -7,12 +7,12 @@ namespace WebApplication1
 
 		public List<Product> listProducts = new List<Product>()
 		{
-			new Product(1, 11, "Name1", "Descr1"),
-			new Product(2, 22, "Name2", "Descr2"),
-			new Product(3, 33, "Name3", "Descr3"),
-			new Product(4, 44, "Name4", "Descr4"),
-			new Product(5, 55, "Name5", "Descr5"),
-			new Product(6, 66, "Name6", "Descr6"),
+			new Product(1, 11, "Name1", "Descr1dfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdf"),
+			new Product(2, 22, "Name2", "Descr2dddddddddddddd"),
+			new Product(3, 33, "Name3", "Descr3ssssssseeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee"),
+			new Product(4, 44, "Name4", "Descr4ddddddd"),
+			new Product(5, 55, "Name5", "Descr5eee"),
+			new Product(6, 66, "Name6", "Descr6eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee"),
 		};
 
 		public List<Product> GetAll()
