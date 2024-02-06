d21ec56ff838937f5317520b8b14ac69ef81d07c
Cawa <Cawa@CAWAKA>
2024-02-06 20:49:25 +0300

    h

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -13,15 +13,13 @@ namespace WebApplication1.Controllers
     {
         private readonly IProductRepository productRepository;
         private readonly ICompareProductDbRepository prodCompare;
-        private readonly IUserAuth user;
         private readonly ICompareProductDbRepository compareProducts;
         private readonly IFavoriteProductDbRepository favoriteProducts;
 
-        public HomeController(IProductRepository productRepository, ICompareProductDbRepository prodCompare, IUserAuth user, ICompareProductDbRepository compareProducts, IFavoriteProductDbRepository favoriteProducts)
+        public HomeController(IProductRepository productRepository, ICompareProductDbRepository prodCompare, ICompareProductDbRepository compareProducts, IFavoriteProductDbRepository favoriteProducts)
         {
             this.productRepository = productRepository;
             this.prodCompare = prodCompare;
-            this.user = user;
             this.compareProducts = compareProducts;
             this.favoriteProducts = favoriteProducts;
         }
@@ -32,114 +30,87 @@ namespace WebApplication1.Controllers
             var newProducts = ProductToProductView.Transform(products);
             var idCompareProds = compareProducts.GetCompareProducts(User.Identity.Name).Select(pr => pr.Id);
             var idFavoriteProds = favoriteProducts.GetFavoriteProducts(User.Identity.Name).Select(pr => pr.Id);
-            //newProducts = newProducts.ForEach(prod =>
-            //{
-            //    if (idCompar)
-            //});
-            //foreach (var product in products)
-            //{
-            //    var prod = new ProductViewModel()
-            //    {
-            //        Id = product.Id,
-            //        Name = product.Name,
-            //        Cost = product.Cost,
-            //        Description = product.Description,
-            //    };
-            //    if (idCompareProds.Contains(product.Id))
-            //    {
-            //        prod.Compare = true;
-            //    }
-            //    if (idFavoriteProds.Contains(product.Id))
-            //    {
-            //        prod.Favorite = true;
-            //    }
-            //    newProducts.Add(prod);
-            //}
+            newProducts.ForEach(prod =>
+            {
+                if (idCompareProds != null)
+                {
+                    if (idCompareProds.Contains(prod.Id))
+                    {
+                        prod.ChangeCompare();
+                    }
+                }
+                if (idFavoriteProds != null)
+                {
+                    if (idFavoriteProds.Contains(prod.Id))
+                    {
+                        prod.ChangeFavorite();
+                    }
+                }
+            });
             return View(newProducts);
         }
         [Authorize]
         public IActionResult AddToComparsion(Guid productId)
         {
-            //prodCompare.Add(productId, user.UserId);
+            prodCompare.Add(productId, User.Identity.Name);
             return RedirectToAction("Index");
         }
         [Authorize]
         public IActionResult Compare()
         {
-            //var products = compareProducts.GetCompareProducts(user.UserId);
-            var prods = new List<ProductViewModel>();
-            //foreach (var item in products)
-            //{
-            //    var newProd = new ProductViewModel();
-            //    newProd.Id = item.Id;
-            //    newProd.Name = item.Name;
-            //    newProd.Cost = item.Cost;
-            //    newProd.Description = item.Description;
-            //    prods.Add(newProd);
-            //}
+            var products = compareProducts.GetCompareProducts(User.Identity.Name);
+            var prods = ProductToProductView.Transform(products);
             return View(prods);
         }
         [Authorize]
         public IActionResult DeleteFromComparsion(Guid productId)
         {
-            //if (UserAuthSession.Auth)
-            //{
-            //    compareProducts.DeleteFromComparsion(productId, user.UserId);
-            //}
+            compareProducts.DeleteFromComparsion(productId, User.Identity.Name);
             return RedirectToAction("Index");
         }
 
         [Authorize]
         public IActionResult AddToFavorite(Guid productId)
         {
-            //if (UserAuthSession.Auth)
-            //{
-            //    favoriteProducts.AddProdToFavor(productId, user.UserId);
-            //}
+            favoriteProducts.AddProdToFavor(productId, User.Identity.Name);
             return RedirectToAction("Index");
         }
         [Authorize]
         public IActionResult DeleteFromFavoriteIndex(Guid productId)
         {
-            //if (UserAuthSession.Auth)
-            //{
-            //    favoriteProducts.DeleteFromFavorite(productId, user.UserId);
-            //}
+            favoriteProducts.DeleteFromFavorite(productId, User.Identity.Name);
             return RedirectToAction("Index");
         }
         [Authorize]
         public IActionResult DeleteFromFavorite(Guid productId)
         {
-            //if (UserAuthSession.Auth)
-            //{
-            //    favoriteProducts.DeleteFromFavorite(productId, user.UserId);
-            //}
+            favoriteProducts.DeleteFromFavorite(productId, User.Identity.Name);
             return RedirectToAction("ShowFavorite");
         }
         [Authorize]
         public IActionResult ShowFavorite()
         {
-            //var prodToShow = favoriteProducts.GetFavoriteProducts(user.UserId);
-            var newListProd = new List<ProductViewModel>();
-            //var idCompareProds = compareProducts.GetCompareProducts(user.UserId).Select(pr => pr.Id);
-            //var idFavoriteProds = favoriteProducts.GetFavoriteProducts(user.UserId).Select(pr => pr.Id);
-            //foreach (var product in prodToShow)
-            //{
-            //    var newProd = new ProductViewModel();
-            //    newProd.Id = product.Id;
-            //    newProd.Cost = product.Cost;
-            //    newProd.Name = product.Name;
-            //    newProd.Description = product.Description;
-            //    if (idCompareProds.Contains(product.Id))
-            //    {
-            //        newProd.Compare = true;
-            //    }
-            //    if (idFavoriteProds.Contains(product.Id))
-            //    {
-            //        newProd.Favorite = true;
-            //    }
-            //    newListProd.Add(newProd);
-            //}
+            var prodToShow = favoriteProducts.GetFavoriteProducts(User.Identity.Name);
+            var newListProd = ProductToProductView.Transform(prodToShow);
+            var idCompareProds = compareProducts.GetCompareProducts(User.Identity.Name).Select(pr => pr.Id);
+            var idFavoriteProds = favoriteProducts.GetFavoriteProducts(User.Identity.Name).Select(pr => pr.Id);
+            newListProd.ForEach(prod =>
+            {
+                if (idCompareProds != null)
+                {
+                    if (idCompareProds.Contains(prod.Id))
+                    {
+                        prod.ChangeCompare();
+                    }
+                }
+                if (idFavoriteProds != null)
+                {
+                    if (idFavoriteProds.Contains(prod.Id))
+                    {
+                        prod.ChangeFavorite();
+                    }
+                }
+            });
             return View(newListProd);
         }
 
