859a83adf1eb6af1f1e5e9e873e5ef672201a1c5
Cawa <Cawa@CAWAKA>
2024-02-15 20:36:50 +0300

    Модель для сохранения изображений

--- WebApplication1/Models/CreateImageViewModel.cs
+++ WebApplication1/Models/CreateImageViewModel.cs
@@ -1,6 +1,6 @@
 ﻿namespace WebApplication1.Models
 {
-    public class CreateViewModel
+    public class CreateImageViewModel
     {
         public Guid Id { get; set; }
         public string Name { get; set; }
